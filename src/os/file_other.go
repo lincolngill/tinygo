556d7f11acb9af5bfb8dea21413aa50bb0ea67da
sago35 <sago35@gmail.com>
2022-02-08 23:48:49 +0900

    Update src/os/file_other.go

--- src/os/file_other.go
+++ src/os/file_other.go
@@ -39,7 +39,8 @@ func NewFile(fd uintptr, name string) *File {
 	return &File{&file{stdioFileHandle(fd), name}}
 }
 
-// Read is unsupported on this system.
+// Read reads up to len(b) bytes from machine.Serial.
+// It returns the number of bytes read and any error encountered.
 func (f stdioFileHandle) Read(b []byte) (n int, err error) {
 	return machine.Serial.Read(b)
 }
